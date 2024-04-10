f83c3064c33511eb28a7dc33be5440515272c76b
Jure Bagic <jurebagic@proton.me>
2024-04-10 23:52:19 +0200

    add transmission-daemon config

---
+++ transmission/.config/transmission-daemon/transmission-handler.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this script is from arch wiki
+percentage=$(transmission-remote -t $TR_TORRENT_ID -l | awk -v ID="$TR_TORRENT_ID" '$0 ~ ID{print $2}')
+
+if [ $percentage != "100%" ]
+then
+     notify-send --app-name="Transmission Started" "Transmission: started $TR_TORRENT_NAME"
+else
+     notify-send --app-name="Transmission Complete" "Transmission: downloaded $TR_TORRENT_NAME"
+fi
